844120f6c159fc34668ce71deaccc14b5b197a6e
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2022-09-02 16:15:37 +0300

    if a model given it will use that models weights and biases otherwise it will generate them randomly

--- layerDense.py
+++ layerDense.py
@@ -2,12 +2,17 @@ import numpy as np
 from activationFuncions import Functions as F
 
 class LayerDense:
-    def __init__(self, n_inputs, n_neurones, activitionFunction): 
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurones) #MxN where M is the # of featues in a single training input and N is the # of desired neuorons
-        self.biases = np.zeros((1, n_neurones))
+    def __init__(self, n_inputs, n_neurones, activitionFunction, weights = None, biases = None):
+        if weights is not None and biases is not None:
+            self.weights = weights
+            self.biases = biases
+        else:
+            self.weights = 0.01 * np.random.randn(n_inputs, n_neurones) #MxN where M is the # of featues in a single training input and N is the # of desired neuorons
+            self.biases = np.zeros((1, n_neurones))
         self.activitionFunction = activitionFunction
+        
+
 
     def ForwordProp(self, inputs): #in this neural network there will be Mx784 input matrix where M is the # of training inputs
         self.dotOutput = np.dot(inputs, self.weights) + self.biases
         self.forwordPropOutput = self.activitionFunction(self.dotOutput)
-
